020abe44a2bf4992d6a4de48cfa254ecb931b241
RigzTech <fredrickarara@gmail.com>
2023-06-15 13:37:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/*
-*C program that prints exactly "Programming is like building a multilingual puzzle
-*/
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle. \n");
-    return 0;
+    puts("\"Programming is like building a multilingual puzzle");
+    return(0);
 }
