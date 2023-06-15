ff7211277e2ea6dba7e41c9fd5804d17e0984d99
RigzTech <fredrickarara@gmail.com>
2023-06-15 15:48:08 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *Description: main - Entry point
+ *
+ * Return: 0 when Success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
