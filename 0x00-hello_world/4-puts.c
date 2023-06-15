06fc1d717415cdf82b71ba1dc6f53fbfb114f659
RigzTech <fredrickarara@gmail.com>
2023-06-15 14:20:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * Entry point 
- * Returns zero
+ * Description:printing out string to stdout. 
+ * Returns:0
 */
+
 int main(void)
+
 {
-    puts ("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return(0);
 }
