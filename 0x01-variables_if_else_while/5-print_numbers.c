7fb438fa01034468a226b6f596850c3794a212a7
RigzTech <fredrickarara@gmail.com>
2023-06-17 03:17:40 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Description: main -for loop
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int x
+		;
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
