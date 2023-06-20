528e6d1b0ce54188c703bfcb3e60ec4ff1894c00
RigzTech <fredrickarara@gmail.com>
2023-06-20 13:50:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <main.h>
 #include "main.c"
 /**
  * Description: main - entry point
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Description: main - print alphabets
+ * return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a', c <= 'z', c++)
+	{
+		 _putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
