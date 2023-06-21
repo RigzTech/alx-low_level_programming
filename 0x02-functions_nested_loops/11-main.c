6a20af922555818bc27bc8c6aa16270412ef9e0c
RigzTech <fredrickarara@gmail.com>
2023-06-21 13:56:49 -0700

    11-main.c

--- 0x02-functions_nested_loops/11-main.c
+++ 0x02-functions_nested_loops/11-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- * Description : main - prints the alphabet, in lowercase
- * Return: Always 0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return (0);
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
+    return (0);
 }
