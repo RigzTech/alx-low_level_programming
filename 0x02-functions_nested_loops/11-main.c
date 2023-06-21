bbe44ec8492be00de9e05e45c134f1ca7f878172
RigzTech <fredrickarara@gmail.com>
2023-06-21 13:47:34 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Description : main - prints the alphabet, in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
