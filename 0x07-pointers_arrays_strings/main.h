d1da1832886e6272ae23934bcfc6b47a2f7bc4a2
RigzTech <fredrickarara@gmail.com>
2023-07-03 13:09:52 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,15 @@
-r - writes the character c to stdout
- * @c: The character to print
- *  *
- *   * Return: On success 1.
- *    * On error, -1 is returned, and errno is set appropriately.
- *     */
- int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _putchar(char);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
